ccb506f1f1830b51a426482e7a5e68405b3d6ed0
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-11-24 12:17:07 -0300

    lunch time

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import SearchForm from "../Search/SearchForm";
 
 function Header() {
 	return (
 		<header>
 			<Navbar bg="light" expand="lg">
-				<Navbar.Brand href="#home">Iron Cooking</Navbar.Brand>
+				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav.Link className="mr-auto">
+					<Nav className="mr-auto">
 						<Nav.Link href="/">Home</Nav.Link>
 						<Nav.Link href="/categories">Categories</Nav.Link>
 						<Nav.Link href="/ingredients">Ingredients</Nav.Link>
 						<Nav.Link href="/cuisines">Cuisines</Nav.Link>
-					</Nav.Link>
+						<Nav.Link href="/searchbyletter/A">A to Z</Nav.Link>
+					</Nav>
 					<SearchForm />
 				</Navbar.Collapse>
 			</Navbar>
