6b4cf98aace0155c4a220c216f6ac199323d8fea
gtretow <guilherme.tretow@gmail.com>
2020-11-24 19:26:56 -0300

    algumas fontes colocadas e botões ajustados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+/* import "./App.css" */
 
 import "../src/styles/bootstrap.css";
 
