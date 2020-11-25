83ec57c91e53c10358b405e3a4f6cd389430e07b
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-11-25 19:39:33 -0300

    Novo layout

---
+++ src/components/Home/Highlights.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+import "./HighLights.css";
+
+function Highlights() {
+	return (
+		<Row className="highContainer">
+			<p>teste</p>
+		</Row>
+	);
+}
+
+export default Highlights;
