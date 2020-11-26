7c59899d6e996a41fb88d300258029d222f183a5
gtretow <guilherme.tretow@gmail.com>
2020-11-26 10:09:50 -0300

    home ajustada

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,24 +3,27 @@ import CarouselDiv from "./CarouselDiv";
 import CategoriesGroup from "./CategoriesGroup";
 import IngredientsGroup from "./IngredientsGroup";
 
-
 function Home() {
-	return (
-		<div>
-				<h2 className="text-center mt-4 pt-3">
-					You got time? We got the recipe!
-				</h2>
-				<h5 className="text-center  pt-3">
-					Start a new adventure today with more than 200 different meals for you
-					to experience
-				</h5>
-				<CarouselDiv />
-				<CategoriesGroup />
-			
-				<IngredientsGroup />
+  return (
+    <div>
+      <h2 className="text-center mt-4 pt-3">
+        You got time? We got the recipe!
+      </h2>
+      <h5 className="text-center  pt-3">
+        Start a new adventure today with almost 300 different meals for you to
+        experience
+      </h5>
+      <CarouselDiv />
+      <br />
+      <br />
+      <div class="dropdown-divider"></div>
+      <CategoriesGroup />
+      <br />
+      <div class="dropdown-divider"></div>
 
-		</div>
-	);
+      <IngredientsGroup />
+    </div>
+  );
 }
 
 export default Home;
