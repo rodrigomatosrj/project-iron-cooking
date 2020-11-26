30233736dcadbe1f37ba3f841b03b478cf237683
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-11-26 09:17:49 -0300

    effect hover

--- src/App.js
+++ src/App.js
@@ -20,40 +20,40 @@ import Favorites from "./components/Favorites/Favorites";
 import "../src/styles/bootstrap.css";
 
 function App() {
-  return (
-    <BrowserRouter className="font">
-      <Header />
-   
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/meal/:id" component={Meal} />
-          <Route exact path="/categories" component={ListCategories} />
-          <Route exact path="/categories/:id" component={ListMealsCategory} />
-          <Route
-            exact
-            path="/ingredients/:title"
-            component={ListMealIngredients}
-          ></Route>
-          <Route exact path="/ingredients" component={ListIngredients}></Route>
-          <Route exact path="/search/:querystring" component={Search}></Route>
-          <Route exact path="/cuisines" component={ListCuisines}></Route>
-          <Route
-            exact
-            path="/cuisines/:area"
-            component={ListMealCuisines}
-          ></Route>
-          <Route exact path="/searchbyletter" component={ListLetters}></Route>
-          <Route
-            exact
-            path="/searchbyletter/:letter"
-            component={ListLetters}
-          ></Route>
-          <Route exact path="/favorites" component={Favorites}></Route>
-        </Switch>
-  
-      <Footer />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter className="font">
+			<Header />
+			<Container>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/meal/:id" component={Meal} />
+					<Route exact path="/categories" component={ListCategories} />
+					<Route exact path="/categories/:id" component={ListMealsCategory} />
+					<Route
+						exact
+						path="/ingredients/:title"
+						component={ListMealIngredients}
+					></Route>
+					<Route exact path="/ingredients" component={ListIngredients}></Route>
+					<Route exact path="/search/:querystring" component={Search}></Route>
+					<Route exact path="/cuisines" component={ListCuisines}></Route>
+					<Route
+						exact
+						path="/cuisines/:area"
+						component={ListMealCuisines}
+					></Route>
+					<Route exact path="/searchbyletter" component={ListLetters}></Route>
+					<Route
+						exact
+						path="/searchbyletter/:letter"
+						component={ListLetters}
+					></Route>
+					<Route exact path="/favorites" component={Favorites}></Route>
+				</Switch>
+			</Container>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/Home/ImageLink.js
+++ src/components/Home/ImageLink.js
@@ -4,21 +4,6 @@ import "./ImageLink.css";
 
 function ImageLink(props) {
 	return (
-		// <div
-		// 	role="radio"
-		// 	aria-checked="false"
-		// 	tabindex="-1"
-		// 	className="foodbox radio-btns__btn shadow-lg bg-secondary mt-5 border border-dark"
-		// >
-		// 	<Link to={`/${props.type}/${props.id}`}>
-		// 		<img className="foodImg" src={props.image} alt={props.title} />
-
-		// 		<div className="middle">
-		// 			<div className="text">{props.title}</div>
-		// 		</div>
-		// 	</Link>
-		// </div>
-
 		<div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 mt-5">
 			<div className="hovereffect">
 				<img className="image" src={props.image} alt={props.title} />
